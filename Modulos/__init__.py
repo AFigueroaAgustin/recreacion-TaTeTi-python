47637c40d8ff908b8cd8d2a5be2c9a9c8b26cfbb
Agustin F <agustinfigueroa390@gmail.com>
2025-06-24 20:04:23 -0300

    🔧 Refactorizado main.py y reorganizado proyecto en módulos - Agregado bloque __main__ para ejecución controlada - Creado archivo __init__.py en carpeta Modulos

---
+++ Modulos/__init__.py
@@ -0,0 +1,4 @@
+from .constantes import DIM, id_equipo
+from .equipos import carga_de_eq1, carga_de_eq2
+from .juego import juego
+from .reporte import informe
---
+++ Modulos/constantes.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# formato de los equipos
+DIM = 10
+id_equipo = np.dtype([
+    ("id_jugador", int),
+    ("nomapo", "U20"),
+    ("pt", int)])
